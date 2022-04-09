19214075cd7c147bf1977d3244a427ab1cee5ee7
zrmin <3547879468@qq.com>
2022-04-09 17:06:49 +0800

    basic template done

--- article/admin.py
+++ article/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# 别忘了导入ArticlePost
+from .models import ArticlePost
+
+# 注册ArticlePost到admin种
+admin.site.register(ArticlePost)
--- article/urls.py
+++ article/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+# 引入views.py
+from . import views
 
 # 正在部署的应用名称
 app_name = 'article'
 
 urlpatterns = [
+    # path函数将url映射到视图
+    path('article-list/', views.article_list, name='article_list'),
 ]
