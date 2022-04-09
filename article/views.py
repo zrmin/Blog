ac54152aa93052b27a78103072531b8eee0949b1
zrmin <3547879468@qq.com>
2022-04-09 21:21:13 +0800

    add markdown to write articles

--- article/views.py
+++ article/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 # 导入数据模型ArticlePost
 from .models import ArticlePost
 
+# 引入markdown模块——>将markdown语法书写的文章渲染为html文本
+import markdown
+
 def article_list(request):
     # 取出所有博客文章
     articles = ArticlePost.objects.all()
@@ -16,7 +19,17 @@ def article_list(request):
 def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
+
+    # 将markdown语法渲染成html样式
+    article.body = markdown.markdown(article.body,
+            extensions=[
+                # 包含 缩写、表格等常用扩展
+                'markdown.extensions.extra',
+                # 语法高亮扩展
+                'markdown.extensions.codehilite',
+                ])
+
     # 需要传递给模板的对象
-    context = {'article': article,}
+    context = {'article': article}
     # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
